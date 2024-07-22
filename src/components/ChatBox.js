3424a0a96c195e5d0e1cc07278276e1bb7ca0681
nptin1996 <tinnpfx21727@funix.edu.vn>
2024-07-22 15:34:57 +0700

    1

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,18 +1,103 @@
-import { useState, useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import { Context } from "../store/context";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import classes from "./ChatBox.module.css";
 import IconSend from "./UI/IconSend";
 import { io } from "socket.io-client";
 import Avatar from "./UI/businessman.png";
+import { fetchData } from "../function";
 function ChatBox() {
   const dataChat = useLoaderData();
-  const [pickedChat, setPickedChat] = useState(null);
   const [chatlist, setChatList] = useState(dataChat);
-
-  console.log(dataChat);
+  const [pickedChat, setPickedChat] = useState(null);
+  const { logout } = useContext(Context);
+  const navigate = useNavigate();
+  console.log(pickedChat);
   const handlePickChat = (chat) => {
     setPickedChat(chat);
   };
+
+  const handleSendChat = async function (e) {
+    e.preventDefault();
+    if (!pickedChat) return e.target.reset();
+    const fd = new FormData(e.target);
+    const content = fd.get("content").trim();
+    if (!content) return e.target.reset();
+    try {
+      const dataSend = {
+        type: "admin",
+        chatId: pickedChat.chatId,
+        content,
+      };
+      console.log(dataSend);
+      const res = await fetchData("chat/admin", "POST", dataSend);
+      if (res.status === 401 || res.status === 403) {
+        logout();
+        return navigate("/login");
+      }
+      if (res.ok) {
+        setPickedChat((state) => {
+          const newChatList = [
+            ...state.chatList,
+            { type: dataSend.type, content: dataSend.content },
+          ];
+          return { ...state, chatList: newChatList };
+        });
+        setChatList((state) => {
+          const chat = state.find((ele) => ele.chatId === dataSend.chatId);
+          const newChatList = [
+            ...chat.chatList,
+            { type: dataSend.type, content: dataSend.content },
+          ];
+          chat.chatList = newChatList;
+          return state;
+        });
+        return;
+      }
+      throw new Error();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const socket = io(process.env.REACT_APP_API_URL);
+    socket.connect();
+    socket.on("chats", (data) => {
+      if (data.action === "Client Send Chat") {
+        setChatList((prevChats) => {
+          const chatIndex = prevChats.findIndex(
+            (chat) => chat.chatId === data.chatId
+          );
+          if (chatIndex !== -1) {
+            // Sao chép các dữ liệu tránh update state bị lỗi mutating
+            const updatedChats = [...prevChats];
+            const updatedChat = { ...updatedChats[chatIndex] };
+            updatedChat.chatList = [...updatedChat.chatList, data.chatData];
+            updatedChats.splice(chatIndex, 1);
+            // Đưa object đó lên đầu mảng
+            return [updatedChat, ...updatedChats];
+          }
+          const newChat = { chatId: data.chatId, chatList: [data.chatData] };
+          return [newChat, ...prevChats];
+        });
+        setPickedChat((state) => {
+          if (state && state.chatId === data.chatId) {
+            const newChat = { ...state };
+            newChat.chatList = [...newChat.chatList, data.chatData];
+            return newChat;
+          } else {
+            return state;
+          }
+        });
+      }
+    });
+
+    return () => {
+      socket.off("chats");
+      socket.disconnect(); // Ngắt kết nối socket
+    };
+  }, []);
 
   return (
     <>
@@ -21,17 +106,21 @@ function ChatBox() {
         <ul className={classes.listUser}>
           {chatlist.map((chat) => (
             <li
-              key={chat.session.chat.chatId}
-              onClick={() => handlePickChat(chat.session.chat)}
+              key={chat.chatId}
+              onClick={() => handlePickChat(chat)}
+              className={
+                pickedChat && pickedChat.chatId === chat.chatId
+                  ? classes.picked
+                  : ""
+              }
             >
               <img src={Avatar} alt="Businessman" />
-              {chat.session.user ? (
+              {chat.user ? (
                 <p>
-                  {chat.session.user.name}{" "}
-                  <span>({chat.session.chat.chatId})</span>
+                  {chat.user.name} <span>({chat.chatId})</span>
                 </p>
               ) : (
-                <p>{chat.session.chat.chatId}</p>
+                <p>{chat.chatId}</p>
               )}
             </li>
           ))}
@@ -59,9 +148,8 @@ function ChatBox() {
               ))}
           </ul>
 
-          <form>
+          <form onSubmit={handleSendChat}>
             <input name="content" type="text" placeholder="Type and enter" />
-            <input name="type" type="hidden" value="admin" />
             <button type="submit">
               <IconSend />
             </button>
